35dbd7fbb0ea84cf456a4db12272409774ae4b78
cchaechae <seo23c@mtholyoke.edu>
2017-11-29 15:14:11 -0500

    initial committ

---
+++ src/kinectUDP/MsgProvider.java
@@ -0,0 +1,11 @@
+package kinectUDP;
+
+import java.util.concurrent.BlockingQueue;
+
+public interface MsgProvider {
+	public void start();
+	public void stop();
+	public boolean isRunning();
+	public BlockingQueue<Msg> getMsgQueue();
+
+}
